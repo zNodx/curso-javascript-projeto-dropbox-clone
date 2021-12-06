3e587584a961522a87c1d3f8b4942177353a6c12
mateus <ffrmateus@gmail.com>
2021-12-06 17:31:12 -0300

    first commit

---
+++ App/routes/index.js
@@ -0,0 +1,106 @@
+var express = require('express');
+var router = express.Router();
+var formidable = require('formidable')
+var fs = require('fs')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/file', (req, res)=>{
+
+  let path = '' + req.query.filepath;
+  console.log(path);
+  if(fs.existsSync(path)){
+
+    fs.readFile(path, (err, data)=>{
+
+      if(err){
+
+        console.error(err);
+        res.status(400).json({
+          error:err
+
+        });
+
+      }else{
+
+        res.status(200).end(data);
+
+      }
+
+    })
+
+  }else{
+
+    res.status(404).json({
+
+      error:'File not found.'
+
+    });
+
+  }
+
+})
+
+router.delete('/file', (req, res) => {
+  let form = new formidable.IncomingForm({
+    uploadDir: './upload',
+    keepExtensions: true
+  })
+
+  form.parse(req, (err, fields, files) => {
+
+    let path = '' + fields.path;
+    let path1 = ''+fields.filepath
+    if (fs.existsSync(fields.filepath)) {
+      console.log(path1);
+      fs.unlink(path1, err => {
+        if (err) {
+          res.status(400).json({
+            err
+          })
+        } else {
+          res.json({
+            fields
+          })
+        }
+      })
+    }
+
+    
+    if (fs.existsSync(fields.path)) {
+    console.log(path);
+      fs.unlink(path, err => {
+        if (err) {
+          res.status(400).json({
+            err
+          })
+        } else {
+          res.json({
+            fields
+          })
+        }
+      })
+    }
+    
+  })
+})
+
+router.post('/upload', (req, res) => {
+
+  let form = new formidable.IncomingForm({
+    uploadDir: './upload',
+    keepExtensions: true
+  })
+
+  form.parse(req, (err, fields, files) => {
+    res.json({
+      files
+    })
+  })
+
+})
+
+module.exports = router;
